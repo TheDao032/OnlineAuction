a4837f66605f2663dffc2244fc37c2bf9d3f52a8
pvtdao <1760278@student.hcmus.edu.vn>
2021-11-04 23:53:50 +0700

    update UI seller manage item

--- client/src/feature/Seller/components/ListPermission/ListPermission.jsx
+++ client/src/feature/Seller/components/ListPermission/ListPermission.jsx
@@ -1,14 +1,25 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { setLoading } from "../../../../redux/actions/loadingAction";
 import "./ListPermission.scss";
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
+import Loading from '../../../../components/Loading/Loading'
+import formatCurrency from '../../../../util/formatCurrency'
+import getFullDay from '../../../../util/getFullDay'
+
 ListPermission.propTypes = {};
 
 function ListPermission(props) {
   const dispatch = useDispatch();
   const { prodId } = useParams();
+
+  const loadingState = useSelector(state => state.loading)
+
+  const [listPermission, setListPermission] = useState([])
+  const [listAuction, setListAuction] = useState([])
+
 
   const {
     user: { accessToken },
@@ -30,9 +41,34 @@ function ListPermission(props) {
         }
       );
 
+      dispatch(setLoading(false));
       console.log(res.data);
+      setListPermission(res.data.listPermission)
+    } catch (error) {
+      dispatch(setLoading(false));
+      console.log(error.response);
+    }
+  }
+
+  async function getListAuction() {
+    dispatch(setLoading(true));
+    try {
+
+      const res = await axios.post(
+        "https://onlineauctionserver.herokuapp.com/api/auction/list-auction",
+        {
+          prodId: +prodId,
+        },
+        {
+          headers: {
+            authorization: accessToken,
+          },
+        }
+      );
 
       dispatch(setLoading(false));
+      console.log(res.data);
+      setListAuction(res.data.listAuctions)
     } catch (error) {
       dispatch(setLoading(false));
       console.log(error.response);
@@ -41,15 +77,80 @@ function ListPermission(props) {
 
   useEffect(() => {
     getListPermission();
+    getListAuction()
   }, []);
 
+  console.log(loadingState)
   return (
     <>
-      <div className="listPermission">
-        <h1 className="listPermission__title">Danh sách đang đấu giá</h1>
-      </div>
+      {loadingState.loading ? <Loading /> :
+        <div className="listPermission">
+          <h1 className="listPermission__title">Danh sách chờ cho quyền</h1>
+          {/* <hr /> */}
+          <div className='listPermission__list'>
+            {
+              listPermission.length === 0
+                ? <p className="listPermission__empty">Chưa có bidder nào đang chờ cấp quyền</p>
+                : listPermission.map(item => {
+                  return (
+                    <ListPermissionItem name={item.perBidderName === '' ? item.perBidderEmail : item.perBidderName} />
+                  )
+                })
+            }
+          </div>
+          <hr />
+          <h1 className="listPermission__title">Danh sách người đấu giá sản phẩm</h1>
+          <div className='listPermission__list'>
+            {
+              listAuction.length === 0
+                ? <p className="listPermission__empty">Chưa có bidder nào đấu giá sản phẩm này</p>
+                : listAuction.map(item => {
+                  return (
+                    <ListAuctionItem
+                      name={item.sttBidderName === null ? item.sttBidderEmail : item.perBidderName}
+                      price={item.sttBiggestPrice}
+                      time={item.createdDate}
+                    />
+                  )
+                })
+            }
+          </div>
+        </div>
+      }
     </>
   );
+}
+
+function ListPermissionItem({ name }) {
+  return (
+    <div className='listPermission__item'>
+      <h6 className='listPermission__item-name'>Bidder: <span>{name}</span> muốn đấu giá sản phẩm này</h6>
+      <div className='listPermission__item-action'>
+        <button className='listPermission__item-btn listPermission__item-btn--yes'>
+          <AiOutlineCheck />
+          <p>Đồng ý</p>
+        </button>
+        <button className='listPermission__item-btn listPermission__item-btn--no'>
+          <AiOutlineClose />
+          <p>Từ chối</p>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function ListAuctionItem({ name, price, time }) {
+  return (
+    <div className='listPermission__item'>
+      <h6 className='listPermission__item-name'>Bidder: <span>{name}</span> đấu giá sản phẩm với giá <span>{formatCurrency(price)}</span> lúc <span>{`${getFullDay(time.split(' ')[0])} - ${(time.split(' ')[1])}`}</span></h6>
+      <div className='listPermission__item-action'>
+        <button className='listPermission__item-btn listPermission__item-btn--no'>
+          <AiOutlineClose />
+          <p>Từ chối</p>
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default ListPermission;
